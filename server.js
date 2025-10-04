c442898732f814b394dcb0f5f873944d5cc7950e
noamk <noamk@noamks-Virtual-Machine.local>
2025-10-05 00:10:44 +0300

    fixed blog update

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('.'));
